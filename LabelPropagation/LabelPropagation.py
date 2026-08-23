b1493505bc8c9b35d82cc347fa75a445ecdccb3e
Bua115 <75591646+Bua115@users.noreply.github.com>
2026-08-23 16:31:36 -0300

    Ambiente e complementos do código
    - Adicionado arquivo com o ambiente Conda de desenvolvimento da rede de propagação
    - Finalizado o código da rede, agora faltando testes e criação dos gráficos com os datasets
    - Adicionado arquivo em formato utilizado no jupyter notebook para testes em um ambiente mais didático e interativo
    - Adicionado arquivos do dataset na pasta "Data"

---
+++ LabelPropagation/LabelPropagation.py
@@ -0,0 +1,47 @@
+import numpy as np
+import random
+#Funcao para a obtencao dos vizinhos
+def ObterVizinhos(N, i):
+  Vizinhos = []
+  for i in range(len(N)):
+    if N[i] == 1:
+      Vizinhos.append(i)
+  return Vizinhos
+
+def CalcularModaEmpateAleatorio(RotulosVizinhos):
+   FrequenciaMaxima = max(np.bincount(RotulosVizinhos))
+   NovoRotulo = random.choice(np.where(np.bincount(RotulosVizinhos) == FrequenciaMaxima)[0])
+   return NovoRotulo 
+
+def LabelPropagation(A, maxIteracoes):
+     #Inicializacao (N = numero de vertices)
+        iteracao = 0
+        N = A
+        Rotulos = np.array([N])
+        # print(Rotulos)
+        RotulosMudaram = True
+    
+        while(iteracao < maxIteracoes and RotulosMudaram):
+            #Aleatorizar ordem dos vertices
+            OrdemVertices = N
+            random.shuffle(OrdemVertices)
+            RotulosMudaram = False
+            # Obter vizinhos e calcular + verificar parada 
+            for i in OrdemVertices:
+                Vizinhos = ObterVizinhos(N, i) 
+                for i in Vizinhos:
+                    if i != []:
+                        RotulosVizinhos = Rotulos[i]
+                        NovoRotulo = CalcularModaEmpateAleatorio(RotulosVizinhos)
+                        #print(NovoRotulo)
+                    if NovoRotulo != Rotulos[i]:
+                      Rotulos[i] = NovoRotulo
+                      RotulosMudaram = True
+            iteracao = iteracao + 1
+        return Rotulos
+
+# Leitura do arquivo com os dados e separação em vetores
+with open(r"..\Data\rede1_duas_comunidades.csv", 'r') as f:
+    Data = [line.strip().split(",") for line in f if line.strip()]
+# print(Data)
+LabelPropagation(Data, 100)
